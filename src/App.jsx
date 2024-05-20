780ed57178b56d3ae73923995a19c9f5bf4fadf3
Sayeed <sksayed13@gmail.com>
2024-05-20 17:23:58 +0400

    Single Job Component
    Recent job to show in homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,17 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobListing from "./components/JobListing"
+import ViewAllJobs from "./components/ViewAllJobs"
 
 const App = () => {
   return (
     <>
       <Navbar />
-
       <Hero title="This is Hero Title" subtitle="This is Hero subtitle" />
-
-
       <HomeCards />
-
       <JobListing />
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+      <ViewAllJobs/>
+      
     </>
   )
 }
